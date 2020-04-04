97c835abcc76dfbef4caad51187e9f10e2f38468
afreencoder <afreen.sft@gmail.com>
2020-04-05 00:14:27 +0530

    first class functions

--- first.js
+++ first.js
@@ -1,3 +1,50 @@
-var a = "afreen";
+// First class functions
 
-console.log(a);
+
+// 1. Anonymous function
+const fruit = function() {
+    console.log("Apple");
+}
+fruit();
+
+
+//2. Named function with parameters
+function sayHello() {
+    return "Hello, ";
+ }
+
+ function helloMessage(){
+     return "Afreen";
+ }
+ function greeting(a, b) {
+   console.log(a() + b());
+ }
+
+ function addition(a, b) {
+    console.log(a + b);
+  }
+
+  function division(a, b) {
+      alert(a/b);
+  }
+
+  function name(a, b) {
+    console.log(a +' ' + b);
+  }
+ // Pass `sayHello` as an argument to `greeting` function
+ //greeting(sayHello, "JavaScript!");
+
+ greeting(sayHello, helloMessage);
+ addition(5,10);
+ name("Afreen","Sultan");
+ division(9,3);
+
+
+
+ // Return a function
+
+const foo = function(){
+    return greeting(sayHello, helloMessage);
+}
+
+foo();
